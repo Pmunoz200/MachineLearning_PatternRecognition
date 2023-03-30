76c116b58edc1fa9475c10a4449fabcfb030aa6d
Pablo Munoz s310308 <s310308@studenti.polito.it>
2023-03-30 07:32:26 +0200

    Added the MLandPattern library to LDA.py

--- Labs/Lab3/LDA.py
+++ Labs/Lab3/LDA.py
@@ -1,15 +1,14 @@
 import numpy as np
-import general_methods
 import scipy
-from matplotlib import pyplot as plt
+from MLandPattern import MLandPattern as ML
 
 
 def covariance_within_class(matrix_values, label, class_labels):
     within_cov = np.zeros((matrix_values.shape[0], matrix_values.shape[0]))
     n = matrix_values.size
     for i in range(len(class_labels)):
-        centered_matrix = general_methods.center_data(matrix_values[:, label == i])
-        cov_matrix = general_methods.covariance(centered_matrix)
+        centered_matrix = ML.center_data(matrix_values[:, label == i])
+        cov_matrix = ML.covariance(centered_matrix)
         cov_matrix = np.multiply(cov_matrix, centered_matrix.size)
         within_cov = np.add(within_cov, cov_matrix)
     within_cov = np.divide(within_cov, n)
@@ -19,11 +18,11 @@ def covariance_within_class(matrix_values, label, class_labels):
 def covariance_between_class(matrix_values, label, class_labels):
     between_cov = np.zeros((matrix_values.shape[0], matrix_values.shape[0]))
     N = matrix_values.size
-    m_general = general_methods.mean_of_matrix_rows(matrix_values)
+    m_general = ML.mean_of_matrix_rows(matrix_values)
     for i in range(len(class_labels)):
         values = matrix_values[:, label == i]
         nc = values.size
-        m_class = general_methods.mean_of_matrix_rows(values)
+        m_class = ML.mean_of_matrix_rows(values)
         norm_means = np.subtract(m_class, m_general)
         matr = np.multiply(nc, np.dot(norm_means, norm_means.T))
         between_cov = np.add(between_cov, matr)
@@ -53,11 +52,11 @@ if __name__ == '__main__':
     pathname = '/Users/pablomunoz/Desktop/Polito 2023-1/MachineLearning/Codes/Labs/data/iris.csv'
     m = 2
 
-    [attributes, labels] = general_methods.load(pathname, class_label, attribute_names)
+    [attributes, labels] = ML.loadCSV(pathname, class_label, attribute_names)
     [Sw, Sb] = between_within_covariance(attributes, labels, class_label)
-    [W, U] = LDA1(attributes, labels, class_label, m)
+    [W, _] = ML.LDA1(attributes, labels, class_label, m)
     P = np.dot(W.T, attributes)
-    general_methods.graphic_scatter_2d(P, labels, class_label)
+    ML.graphic_scatter_2d(P, labels, class_label)
 
 
 
